1965130c2da0bd806789161bfb2fa104e920d2fe
jasenkaz <jasenkaz@yahoo.com>
2017-07-05 20:58:13 -0400

    angular app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const angular = require('angular');
 
+console.log(angular);
+
+//create application
+const app = angular.module('darkcaster-client', []); //setter syntax: creates a new module, gives it name, and adds [] so angular knows we created something new; in the long term use the brackets to add powerapps like animations ie ngAnimate from angular
+
+app.controller('MainController', MainController);
+
+MainController.$inject = ['$scope']; //find $scope and put it below as variable
+
+function MainController($scope){
+  $scope.message = 'hello from angular';  //html only knows this message that is under $scope; $scope links JS and html.
+}
