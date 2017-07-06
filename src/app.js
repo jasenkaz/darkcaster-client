d31ac67aea75b42239e13934c1da4c92e52b052b
jasenkaz <jasenkaz@yahoo.com>
2017-07-06 18:49:17 -0400

    added built script and refactored to use getter syntax in angular JS and removed the need for $scope

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const angular = require('angular');
 console.log(angular);
 
 //create application
-const app = angular.module('darkcaster-client', []); //setter syntax: creates a new module, gives it name, and adds [] so angular knows we created something new; in the long term use the brackets to add powerapps like animations ie ngAnimate from angular
+angular.module('darkcaster-client', []); //setter syntax: creates a new module, gives it name, and adds [] so angular knows we created something new; in the long term use the brackets to add powerapps like animations ie ngAnimate from angular
 
-app.controller('MainController', MainController);
+angular.module('darkcaster-client') //getter synatx, grabbing the module and calling .controller to register the conntroller; angular.module is angular version of require
+       .controller('MainController', MainController); //this is how angular figures out what we are talking about
 
-MainController.$inject = ['$scope']; //find $scope and put it below as variable
+MainController.$inject = []; //find $scope and put it below as variable; dependenci injection; dependent on $scope, and finds it below; link between HTLM and JS; but we will use this. instead
 
-function MainController($scope){
-  $scope.message = 'hello from angular';  //html only knows this message that is under $scope; $scope links JS and html.
+function MainController(){
+  this.message = 'hello from angular';  //html only knows this message that is under $scope; $scope links JS and html.
 }
