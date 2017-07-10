4e3942188a2a349d6b96f526e958abcb0fb91f67
jasenkaz <jasenkaz@yahoo.com>
2017-07-10 13:26:18 -0400

    adding images

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 /*
  * We've enabled UglifyJSPlugin for you! This minifies your app
  * in order to load faster and run less javascript.
@@ -65,5 +66,8 @@ module.exports = {
 							title: 'Darkcaster',
 							filename: 'index.html',
 							template: './src/index.html'
-						})]
+						}),
+					new CopyWebpackPlugin([
+						{from: './src/images' to: './images'}
+					])]
 };
